ae207ffc5c810eaac224547f93c00189ce18fc97
Arunkumar .s.j. <zack.yt.7085@gmail.com>
2025-12-29 22:12:26 +0530

    Update AIHandler.js

--- AIHandler/AIHandler.js
+++ AIHandler/AIHandler.js
@@ -1,3 +1,4 @@
+// AIHandler/AIHandler.js
 import { AISyntax } from './AISyntax.js';
 import { AILoader } from './AILoader.js';
 
@@ -6,36 +7,10 @@ export class AIHandler {
         this.syntax = new AISyntax();
         this.loader = new AILoader();
         
-        // Expose syntax methods for easy access
+        // Mapping for easy access
         this.txt = this.syntax.txt;
         this.img = this.syntax.img;
-        this.vid = this.syntax.vid;
-        this.snd = this.syntax.snd;
-        this.sfx = this.syntax.sfx;
         this.set = this.syntax.set;
-        this.attach = this.syntax.attach;
-        this.models = this.syntax.models;
-    }
-
-    async generate() {
-        return await this.loader.load(this.syntax.config);
-    }
-}import { AISyntax } from './AISyntax.js';
-import { AILoader } from './AILoader.js';
-
-export class AIHandler {
-    constructor() {
-        this.syntax = new AISyntax();
-        this.loader = new AILoader();
-        
-        // Expose syntax methods for easy access
-        this.txt = this.syntax.txt;
-        this.img = this.syntax.img;
-        this.vid = this.syntax.vid;
-        this.snd = this.syntax.snd;
-        this.sfx = this.syntax.sfx;
-        this.set = this.syntax.set;
-        this.attach = this.syntax.attach;
         this.models = this.syntax.models;
     }
 
